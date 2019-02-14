4d4d836a0b0739f9c6a2f47c29e1bd501584bd81
Peng Xiang <hi.pxiang@gmail.com>
2019-02-14 16:31:36 +0800

    add cut text scripts

---
+++ tools/oneKey.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+file=me.txt
+if [[ "$1" != "" ]]; then
+    file=$1
+fi
+# del empty line
+sed -i '/^$/d' $file
+# del start space
+sed -i 's/^[ \t]*//' $file
+# del trailing space
+sed -i 's/[ \t]*$//' $file
+# del start and trailing comma etc
+sed -i 's/^[，。；,.;]//g' $file
+sed -i 's/[，。；,.;]$//g' $file
+# replace all full stop to comma
+sed -i 's/[。，]/ /g' $file
