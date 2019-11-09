40f8ec8534b1a201113c63a458b2d2d07fb93805
Peng Xiang <hi.pxiang@gmail.com>
2019-11-09 17:39:29 +0800

    minor update

--- tools/oneKey.sh
+++ tools/oneKey.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# for subitile words in every line no more than 50
 file=go.txt
 if [[ "$1" != "" ]]; then
     file=$1
@@ -13,7 +14,7 @@ sed -i 's/[ \t]*$//' $file
 sed -i 's/^[，。；,.;]//g' $file
 sed -i 's/[，。；,.;]$//g' $file
 # replace all period and comma to space
-sed -i 's/[。，]/ /g' $file
+sed -i 's/[。，、]/ /g' $file
 # del pos mark
 sed -i 's/【.*】//g' $file
 sed -i 's/[.*]//g' $file
