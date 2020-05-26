09e3657fcdd0776b76ff16422338bc578fa19bd1
Peng Xiang <hi.pxiang@gmail.com>
2020-05-26 19:32:27 +0800

    add functions for onekey

--- tools/oneKey.sh
+++ tools/oneKey.sh
@@ -1,20 +1,70 @@
 #!/bin/bash
 # each line no more than 50 char
+
 file=go.txt
-if [[ "$1" != "" ]]; then
-    file=$1
-fi
-# del empty line
-sed -i '/^$/d' $file
-# del start space
-sed -i 's/^[ \t]*//' $file
-# del trailing space
-sed -i 's/[ \t]*$//' $file
-# del start and trailing comma etc
-sed -i 's/^[，。；,.;]//g' $file
-sed -i 's/[，。；,.;]$//g' $file
-# replace all period and comma with space
-sed -i 's/[。，、]/ /g' $file
-# del pos mark
-sed -i 's/【.*】//g' $file
-sed -i 's/[.*]//g' $file
+# if [[ "$1" != "" ]]; then
+#     file=$1
+# fi
+
+pringUsage() {
+    exeName=${0##*/}
+    cat << _EOF
+[NAME]
+    $exeName -- a tiny tool for lazy man using FinalCut Pro
+
+[SYNOPSIS]
+    sh $exeName [help | arctime | notes]
+
+[EXAMPLE]
+    sh $exeName
+    sh $exeName notes
+    sh $exeName help
+
+[DESCRIPTION]
+    help  -- print the help messages
+    notes -- add extra blank lines between each lines
+    arctime -- adjust format to fit its use in arctime
+
+_EOF
+}
+
+forArcTime() {
+    # del empty line
+    sed -i '/^$/d' $file
+    # del start space
+    sed -i 's/^[ \t]*//' $file
+    # del trailing space
+    sed -i 's/[ \t]*$//' $file
+    # del start and trailing comma etc
+    sed -i 's/^[，。；,.;]//g' $file
+    sed -i 's/[，。；,.;]$//g' $file
+    # replace all period and comma with space
+    sed -i 's/[。，、]/ /g' $file
+    # del pos mark
+    sed -i 's/【.*】//g' $file
+    sed -i 's/[.*]//g' $file
+}
+
+addExtraLinesBetEachLine() {
+    # Solu 1: sed command
+    # sed -i 's/\n\@<!\n\n\@!/\r\r/g' $file
+
+    Solu 2: vim command
+    vim $file +"%s/\n\@<!\n\n\@!/\r\r/g" +"w" +"qa"
+}
+
+case $1 in
+    'usage' | 'help')
+        pringUsage
+        ;;
+
+    'notes')
+        set -x
+        addExtraLinesBetEachLine
+        ;;
+
+    'arctime' | *)
+        forArcTime
+        exit
+        ;;
+esac
